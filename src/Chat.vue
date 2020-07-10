321dbac8680bad0039540d4b1c5223c931ad5112
areph <40balmung@gmail.com>
2020-07-10 18:32:39 +0900

    Chatアプリの雛形を作成

--- src/Chat.vue
+++ src/Chat.vue
@@ -1,15 +1,81 @@
 <template>
   <div>
     <div class="title">チャットアプリ</div>
+    <div class="main-contents">
+      <div v-for="message in messages" :key="message.id">
+        <div
+          v-bind:class="[
+            message.username === userName ? 'message' : 'message_opponent',
+          ]"
+        >
+          {{ message.content }}
+        </div>
+      </div>
+      <div
+        v-bind:class="[
+          message.username === userName ? 'username' : 'username_opponent',
+        ]"
+      >
+        {{ message.username }}
+      </div>
+    </div>
+    <el-input
+      placeholder="メッセージを入力(Enterで送信)"
+      v-model="content"
+      @keydown.enter.native="sendMessage"
+    ></el-input>
+    <div class="error">{{ this.error }}</div>
   </div>
 </template>
 
-<script>
-  export default {
+<script lang="ts">
+import API, { graphqlOperation } from '@aws-amplify/api';
+import { Auth } from "aws-amplify";
+import { createMessage } from "./graphql/mutations";
+import { listMessages } from './graphql/queries';
+import { onCreateMessage } from "./graphql/subscriptions";
+import { Component, Vue } from "vue-property-decorator";
 
-  }
+@Component
+export default class Chat extends Vue {
+    name = "chat";
+    messages = [];
+    content = '';
+    userName = '';
+    subscription = {};
+    error = '';
+
+    sendMessage() {
+
+    }
+
+    fetch() {
+
+    }
+
+    subscribe() {
+
+    }
+
+    scrollBottom() {
+      const container = this.$el.querySelector('.message_base');
+      if (container) {
+        container.scrollTop = container.scrollHeight;
+      }
+    }
+
+    async created() {
+      this.userName = (await Auth.currentAuthenticatedUser()).username;
+      this.fetch();
+      this.subscribe();
+    }
+
+    beforeDestroy() {
+
+    }
+
+    // updated = scrollBottom();
+}
 </script>
 
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
