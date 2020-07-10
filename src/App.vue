fc14abaac686ceb2c688e231f0d7712a120836ff
areph <40balmung@gmail.com>
2020-07-10 16:29:39 +0900

    Amplify authを導入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <amplify-authenticator v-bind:authConfig="authConfig" />
     <div id="nav">
       <router-link to="/ai">AIアプリ</router-link> |
       <router-link to="/chat">Chatアプリ</router-link> |
@@ -11,3 +12,19 @@
 </template>
 
 <style src="./assets/style.css" />
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  data() {
+    return {
+      authConfig: {
+        signupConfig: {
+          defaultCountryCode: "44",
+          hiddenDefaults: ['phone_number']
+        }
+      }
+    }
+  }
+})
+</script>
